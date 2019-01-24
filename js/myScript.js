2bc193a276b2a08228ece10f0e973dd961654c85
Deeptanshu <deeptanshu.goel@soprasteria.com>
2019-01-24 14:27:38 +0530

    hitting server over app

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,6 @@
 function myFunction(val) {
   if(val=="CRprop"){
-    document.getElementById("form").action = "demo.php";
+    document.getElementById("form").action = "https://demo987456321.000webhostapp.com/plm_app/demo.php";
   }else if(val=="listFeat"){
     document.getElementById("form").action = "index2.html";
   }
