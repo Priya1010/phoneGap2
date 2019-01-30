4141f7d864925e18a4a5a0716ca04c834b399970
EMEAAD\prigupta <priya.gupta@soprasteria.com>
2019-01-30 15:25:45 +0530

    css changed

--- js/myScript.js
+++ js/myScript.js
@@ -1,15 +1,6 @@
 
 window.onload=function(){
   document.getElementById("page2").style.display='none';
-    
-	var frmvalidator = new Validator("form1");
-	frmvalidator.addValidation("name","req","Please enter CR name");
-	frmvalidator.addValidation("name","maxlen=20",
-		"Max length for name is 20");
-
-	frmvalidator.addValidation("owner","req");
-	frmvalidator.addValidation("owner","maxlen=20");
-
 }
 
 
@@ -17,7 +8,7 @@ function myFunction(val) {
   if(val=="CRprop"){
 	  window.location.href = "List.html";
   }else if(val=="listFeat"){
-
+	
 	    var cells = document.getElementsByClassName("disappear");
 		for (var i = 0; i < cells.length; i++) {
 		cells[i].setAttribute("readonly", "readonly");
